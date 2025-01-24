8a8015829d365377a21fea81ccc20cf4ed89320c
kayla <jadefoxx01@gmail.com>
2025-01-23 21:14:04 -0500

    Added commission section

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const navAbout = document.getElementById('about-item');
 const navGraphic = document.getElementById('graphic-item');
 const navUI = document.getElementById('ui-item');
 const navIllus = document.getElementById('illustration-item');
+const navFanart = document.getElementById('fanart-item');
 const navConcept = document.getElementById('concept-item');
 const navContact = document.getElementById('contact-item');
 const checkbox = document.getElementById('check')
@@ -12,6 +13,7 @@ navAbout.addEventListener("click", uncheck);
 navGraphic.addEventListener("click", uncheck);
 navUI.addEventListener("click", uncheck);
 navIllus.addEventListener("click", uncheck);
+navFanart.addEventListener("click", uncheck);
 navConcept.addEventListener("click", uncheck);
 navContact.addEventListener("click", uncheck);
 
