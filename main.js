60e06362795c845508312c5694b08434532e0a14
kayla <jadefoxx01@gmail.com>
2025-03-23 21:19:10 -0400

    Add expandable images on click

--- main.js
+++ main.js
@@ -23,6 +23,32 @@ function uncheck() {
 
 uncheck();
 
+let modal = document.getElementById("popup");
+
+let modalImage = document.getElementById("modal-image");
+
+let closeButton = document.getElementsByClassName("close")[0];
+
+let images = document.querySelectorAll(".expandable");
+
+images.forEach(function(image) {
+    image.onclick = function (e) {
+        let src = e.target.src; 
+        modal.style.display = "block";  
+        modalImage.src = src; 
+    };
+});
+
+closeButton.onclick = function() {
+    modal.style.display = "none";
+};
+
+window.onclick = function(event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+};
+
 if(checkbox.checked == true) {
     //hide scrollbar
 }
