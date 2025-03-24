241642914a130b6bd418ed89286fee1369de0003
kayla <jadefoxx01@gmail.com>
2025-03-24 02:12:21 -0400

    Change about text, add new work imgs, remove blender/graphic section

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const navWelcome = document.getElementById('welcome-item');
 const navAbout = document.getElementById('about-item');
-const navGraphic = document.getElementById('graphic-item');
 const navIllus = document.getElementById('illustration-item');
 const navFanart = document.getElementById('fanart-item');
 const navConcept = document.getElementById('concept-item');
@@ -9,7 +8,6 @@ const checkbox = document.getElementById('check')
 
 navWelcome.addEventListener("click", uncheck);
 navAbout.addEventListener("click", uncheck);
-navGraphic.addEventListener("click", uncheck);
 navIllus.addEventListener("click", uncheck);
 navFanart.addEventListener("click", uncheck);
 navConcept.addEventListener("click", uncheck);
